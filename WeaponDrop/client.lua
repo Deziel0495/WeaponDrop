01c05a7010567c997b702217465dd04a0265b3c5
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-22 01:38:32 -0300

    Update client.lua

--- WeaponDrop/client.lua
+++ WeaponDrop/client.lua
@@ -21,11 +21,12 @@ end
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(0) 
-            if IsPedArmed(GetPlayerPed(-1), 6) then
-	    DisableControlAction(1, 140, true)
-            DisableControlAction(1, 141, true)
-            DisableControlAction(1, 142, true)
+        Citizen.Wait(0)
+		local ped = GetPlayerPed( -1 )
+            	if IsPedArmed(ped, 6) then
+	    	DisableControlAction(1, 140, true)
+            	DisableControlAction(1, 141, true)
+            	DisableControlAction(1, 142, true)
         end
     end
 end)
