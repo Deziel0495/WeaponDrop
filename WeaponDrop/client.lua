8dfa58487da44be5b2ee946854ade3ba1a67f319
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-24 22:16:16 -0300

    Update client.lua

--- WeaponDrop/client.lua
+++ WeaponDrop/client.lua
@@ -1,3 +1,10 @@
+-- Created by Deziel0495 --
+
+-- NOTICE
+-- This script is licensed under "No License". https://choosealicense.com/no-license/
+-- You are allowed to: Download, Use and Edit the Script. 
+-- You are not allowed to: Copy, re-release, re-distribute it without my written permission.
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
